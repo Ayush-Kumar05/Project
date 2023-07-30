b52482c4b86762df1f20c0a95c6560adc8d59ba6
Ayush <119432449+Ayush-Kumar05@users.noreply.github.com>
2023-07-30 23:46:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Mycomponents/Header";
+
+function App() {
+  return (
+  <>
+  <Header/>
+  
+  <h4>E-COURSE PLATFORM</h4>
+  <p>Learning and Doubt Clearing Now Made Easy</p>
+ </>
+  );
+}
+
+export default App;
